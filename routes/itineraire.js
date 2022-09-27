47c360e92b677545c7a913b0721c285c8dd320b2
Rémi PONCHE <ponche62880@gmail.com>
2022-09-27 21:42:13 +0200

    chore: création des routes pour tester API 💩
    création des routes qui permet de récuperer la liste des itinéraires
    et un itinéraire en particulier. ce code doit etre modifier.

--- routes/itineraire.js
+++ routes/itineraire.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ITINERAIRES = require('../models/mock-itineraire.json') //mock itinéraire
 
 /* CRUD de la ressource itineraire */
 
@@ -8,15 +9,17 @@ router.get('/', function(req, res, next) {
     // route qui permet de récuperer tous les itinéraire
 
     // TODO
-  res.json({ title: 'Express' });
+  res.json(ITINERAIRES);
 });
 
 /* GET  un itinéraire itineraire. */
 router.get('/:id', function(req, res, next) {
     // route qui permet de récupéré un itinéraire
+    
+    const itineraire = ITINERAIRES.find(element => element.id == req.params.id) 
 
     // TODO 
-  res.json({ title: 'Express' });
+  res.json(itineraire);
 });
 
 module.exports = router;
