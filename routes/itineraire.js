c1a7213a1306926dca4185ef1a6459d51ec9118b
Rémi PONCHE <ponche62880@gmail.com>
2022-09-28 19:53:36 +0200

    docs: correction fautes de français :bulb:
    correction de fautes de français dans les commentaires d'un fichier

--- routes/itineraire.js
+++ routes/itineraire.js
@@ -4,17 +4,17 @@ var ITINERAIRES = require('../models/mock-itineraire.json') //mock itinéraire
 
 /* CRUD de la ressource itineraire */
 
-/* GET tout les itineraire. */
+/* GET tout les itineraires. */
 router.get('/', function(req, res, next) {
-    // route qui permet de récuperer tous les itinéraire
+    // route qui permet de récuperer tous les itinéraires
 
     // TODO
   res.json(ITINERAIRES);
 });
 
-/* GET  un itinéraire itineraire. */
+/* GET  un itinéraire. */
 router.get('/:id', function(req, res, next) {
-    // route qui permet de récupéré un itinéraire
+    // route qui permet de récupérer un itinéraire
     
     const itineraire = ITINERAIRES.find(element => element.id == req.params.id) 
 
