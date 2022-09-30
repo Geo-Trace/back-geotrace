71f62c30eeb3a1d2412e0d6ba06cb77c0eecbf7c
Rémi PONCHE <ponche62880@gmail.com>
2022-09-30 15:49:22 +0200

    feat: connexion BDD pour la route GET /itineraire :construction:

--- routes/itineraire.js
+++ routes/itineraire.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var ITINERAIRES = require('../models/mock-itineraire.json') //mock itinéraire
+var { MongoClient } = require('mongodb')
 
 /* CRUD de la ressource itineraire */
 
 /* GET tout les itineraires. */
-router.get('/', function(req, res, next) {
-    // route qui permet de récuperer tous les itinéraires
+router.get('/', function (req, res, next) {
+  // route qui permet de récuperer tous les itinéraires
 
-    // TODO
-  res.json(ITINERAIRES);
+  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) throw err
+
+    const db = client.db('database-geotrace')
+
+    db.collection('itineraire').find().toArray((err, result) => {
+      if (err) throw err
+
+      console.log(result)
+      res.json(result)
+    })
+  })
+
+  // TODO
+  //res.json(ITINERAIRES);
 });
 
 /* GET  un itinéraire. */
-router.get('/:id', function(req, res, next) {
-    // route qui permet de récupérer un itinéraire
-    
-    const itineraire = ITINERAIRES.find(element => element.id == req.params.id) 
+router.get('/:id', function (req, res, next) {
+  // route qui permet de récupérer un itinéraire
 
-    // TODO 
+  const itineraire = ITINERAIRES.find(element => element.id == req.params.id)
+
+  // TODO 
   res.json(itineraire);
 });
 
