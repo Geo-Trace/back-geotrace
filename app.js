2f25fef18a8e779679db3ca1d00284fe6dc0e7cc
Rémi PONCHE <ponche62880@gmail.com>
2022-10-06 10:06:11 +0200

    feat: implémentation des metrics dans express :chart_with_upwards_trend:

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const promMid = require('express-prometheus-middleware'); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
