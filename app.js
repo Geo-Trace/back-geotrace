be63e08e852212ce42b7f6ea313b44876ecf639e
Rémi PONCHE <ponche62880@gmail.com>
2022-10-06 15:57:59 +0200

    feat: mise en place du CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 const promMid = require('express-prometheus-middleware'); 
 
 var indexRouter = require('./routes/index');
@@ -12,6 +13,7 @@ var avisRouter = require('./routes/avis')
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
