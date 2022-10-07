c33b5186e84e3310f26988805cadc5912181a728
Rémi PONCHE <ponche62880@gmail.com>
2022-10-07 11:53:45 +0200

    fix: résoudre le problème de plantage de API
    les try/catch était mal fait, pas assez englobant

--- routes/itineraire.js
+++ routes/itineraire.js
@@ -7,45 +7,57 @@ const { MongoClient, ObjectId } = require('mongodb')
 /* GET tout les itineraires. */
 router.get('/', function (req, res, next) {
   // route qui permet de récuperer tous les itinéraires
-
-  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    if (err) throw err
-
-    const db = client.db('database-geotrace')
-
-    db.collection('itineraire').find().toArray((err, result) => {
+  try {
+    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
       if (err) throw err
-
-      console.log(result)
-      res.json(result)
+  
+      const db = client.db('database-geotrace')
+  
+      db.collection('itineraire').find().toArray((err, result) => {
+        if (err) throw err
+  
+        console.log(result)
+        res.json(result)
+      })
     })
-  })
+
+  }
+  catch {
+    res.status(500).json({message: "Erreur serveur"}); 
+  }
+
 
 });
 
 /* GET  un itinéraire. */
 router.get('/:id', function (req, res, next) {
   // route qui permet de récupérer un itinéraire
-
-  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-    if (err) throw err
-
-    try {
-      const db = client.db('database-geotrace')
+  try {
+    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+      if (err) throw err
   
-      db.collection('itineraire').findOne({_id: ObjectId(req.params.id)}).then((result) => {
-        if (err) throw err
+      try {
+        const db = client.db('database-geotrace')
+    
+        db.collection('itineraire').findOne({_id: ObjectId(req.params.id)}).then((result) => {
+          if (err) throw err
+    
+          console.log(result)
+          res.json(result)
+        })
   
-        console.log(result)
-        res.json(result)
-      })
+      }
+      catch {
+        res.status(404).json({message: "Ressource non trouvé"}); 
+      }
+  
+    });
 
-    }
-    catch {
-      res.status(404).json({message: "Ressource non trouvé"}); 
-    }
+  }
+  catch {
+    res.status(500).json({message: "erreur serveur"}); 
+  }
 
-  });
 
 });
 
